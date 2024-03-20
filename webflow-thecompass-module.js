3a543c69a9c99035e0ff44d88861ba5db063e33c
zaeval <zaeval@the-compass.kr>
2024-03-21 08:35:52 +0900

    webflow-thecompass-module.js data-vind hotfix 1.11

--- webflow-thecompass-module.js
+++ webflow-thecompass-module.js
@@ -62,6 +62,7 @@ function dfs(dom) {
                         window[key] = Ref(0)
                     }
                     const realKey = Object.keys(Object.assign(window)).find(t => key.indexOf(t) !== -1)
+                    console.log(realKey)
                     if(realKey) {
                         window[realKey].subscribe((value, args) => {
                             const retValue = eval(key.replaceAll(realKey,realKey+".value "))
