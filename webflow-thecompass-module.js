3fec67c5bc869ad7d55133b12299d99330b13bdb
zaeval <zaeval@the-compass.kr>
2024-03-20 23:16:55 +0900

    webflow-thecompass-module.js data-vind

--- webflow-thecompass-module.js
+++ webflow-thecompass-module.js
@@ -46,7 +46,7 @@ function dfs(dom) {
             dfs(child)
         })
     } else {
-        console.log("dom", dom)
+        // console.log("dom", dom)
         if (dom.innerText) {
             const templates = dom.innerText.match(/\{\{ [\w]+ \}\}/g)
             if (templates) {
@@ -66,26 +66,26 @@ function dfs(dom) {
                 })
             }
         }
-        dom.getAttributeNames().forEach(name => {
-            const templates = dom.getAttribute(name).match(/\{\{ [\w]+ \}\}/g)
-            if (templates) {
-                templates.forEach((template) => {
-                    const key = template.replaceAll("{", "").replaceAll("}", "").replaceAll(" ", "")
-                    if (tc[key] === undefined) {
-                        tc[key] = Ref(0)
-                    }
-                    tc[key].subscribe((value, args) => {
-                        // console.log(value, template, dom.innerText.indexOf(template) === -1)
-                        if (dom.getAttribute(name).indexOf(template) === -1) {
-                            dom.setAttribute(name, args.prop.replaceAll(template, tc[key].value))
-                        } else {
-                            dom.setAttribute(name, dom.getAttribute(name).replaceAll(template, tc[key].value))
-                        }
-                    }, {prop: dom.getAttribute(name)})
-                })
-            }
-        })
     }
+    dom.getAttributeNames().forEach(name => {
+        const templates = dom.getAttribute(name).match(/\{\{ [\w]+ \}\}/g)
+        if (templates) {
+            templates.forEach((template) => {
+                const key = template.replaceAll("{", "").replaceAll("}", "").replaceAll(" ", "")
+                if (tc[key] === undefined) {
+                    tc[key] = Ref(0)
+                }
+                tc[key].subscribe((value, args) => {
+                    // console.log(value, template, dom.innerText.indexOf(template) === -1)
+                    if (dom.getAttribute(name).indexOf(template) === -1) {
+                        dom.setAttribute(name, args.prop.replaceAll(template, tc[key].value))
+                    } else {
+                        dom.setAttribute(name, dom.getAttribute(name).replaceAll(template, tc[key].value))
+                    }
+                }, {prop: dom.getAttribute(name)})
+            })
+        }
+    })
     return
 }
 
